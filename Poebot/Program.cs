6ed5d855137994747a31cdf9f243c6392f7759ff
Nikita Bobylev <012_011@mail.ru>
2019-12-10 18:05:20 +0700

    Реализован класс Program у поебота для дебага

--- Poebot/Program.cs
+++ Poebot/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 
 namespace Poebot
 {
@@ -10,6 +10,24 @@ namespace Poebot
     {
         static void Main(string[] args)
         {
+            Poewatch poewatch = new Poewatch();
+            while (true)
+            {
+                Poebot poebot = new Poebot(poewatch);
+                string query = Console.ReadLine();
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                Message message = poebot.ProcessRequest(query);
+                sw.Stop();
+                Console.WriteLine(message.Text);
+                if (message.Image != null)
+                    using (MemoryStream stream = new MemoryStream(message.Image))
+                    {
+                        Bitmap bmp = new Bitmap(stream); 
+                        bmp.Save("image.png", ImageFormat.Png);
+                        bmp.Dispose();
+                    }
+            }
         }
     }
 }
