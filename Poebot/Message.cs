2af915b061b5e6f8fcd57a1a2207bf814ad637d7
Nikita Bobylev <012_011@mail.ru>
2020-05-16 11:43:26 +0700

    Улучшение класса Message

--- Poebot/Message.cs
+++ Poebot/Message.cs
@@ -4,15 +4,12 @@
     {
         public string Text { get; }
 
-        public AbstractPhoto Photo;
+        public readonly AbstractPhoto Photo;
 
-        public string SysInfo { get; }
-
-        public Message(string text = null, AbstractPhoto photo = null, string sysInfo = "")
+        public Message(string text = null, AbstractPhoto photo = null)
         {
             Text = text;
             Photo = photo;
-            SysInfo = sysInfo;
         }
     }
 }
